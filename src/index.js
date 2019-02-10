94be5eb14321edfd6a424798a71d49f55bf9cb23
PC home 1 <eee1@i.ua>
2019-02-10 14:08:25 +0200

    C to/from F calc2, ver 0.2.0 (J210)

--- src/index.js
+++ src/index.js
@@ -7,4 +7,5 @@ ReactDOM.render(<App.NameForm />, document.getElementById('form'));
 //ReactDOM.render(<App.Textareas />, document.getElementById('textarea'));
 ReactDOM.render(<App.EssayForm />, document.getElementById('textarea2'));
 ReactDOM.render(<App.Calculator />, document.getElementById('calc'));
+ReactDOM.render(<App.Calculator2 />, document.getElementById('calc2'));
 //ReactDOM.render(<App.Editor name={this.state.fileData} />, document.getElementById('edit'));
