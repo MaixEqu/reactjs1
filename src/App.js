b8aa5ed5fb64ba2c91bda53b15ea797f58a990bc
PC home 1 <eee1@i.ua>
2019-02-10 13:00:55 +0200

    tests, ver 0.1.4, J210

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
 import React, { Component } from 'react';
 
-export default class App extends Component {
+export class Header extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <div>[ Learn React (Mx, ver 0.1.3, J210) ]</div>
+          <p id="text">[ Learn React (Mx, ver 0.1.4, J210) ]</p>
+          <div id="edit">edit 02</div>
         </header>
       </div>
     );
   }
 }
+
+/*
+export var Editor = React.createClass({
+  displayName: 'Editor',
+  propTypes: {
+    name: React.PropTypes.string.isRequired
+  },
+  getInitialState: function() { 
+    return {
+      value: this.props.name
+    };
+  },
+  handleChange: function(event) {
+    this.setState({value: event.target.value});
+  },
+  render: function() {
+    return (
+      <form id="noter-save-form" method="POST">
+        <textarea id="noter-text-area" name="textarea" value={this.state.value} onChange={this.handleChange} />
+        <input type="submit" value="Save" />
+      </form>
+    );
+  }
+});
+*/
